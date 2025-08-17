6cbedbb3f0beb8816c548cd362df3b87ddd42d18
jmdez20 <Justinmendez_@outlook.com>
2025-08-17 10:26:34 +0100

    Update firstDuplicate.js
    I've updated the firstDuplicate.js file with a new way of solving the problem in a one liner.

--- arrays/firstDuplicate.js
+++ arrays/firstDuplicate.js
@@ -2,9 +2,15 @@ const firstDuplicate = (arr) => {
     let seen = new Set();
     for (let i = 0; i < arr.length; i++) {
         if (seen.has(arr[i])) {
-            return arr[i];
+            return true;
         }
         seen.add(arr[i]);
     }
-    return -1;
+    return false;   // I just changed the return values
 }; 
+
+// I found a way to make this a lot shorter, into a one-liner. Check below ⬇️
+
+function containsDuplicate(nums) {
+    return new Set(nums).size !== nums.length; 
+};
