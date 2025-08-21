738a57db56c196eb9984255e206e3e8ac5067c42
jmdez20 <Justinmendez_@outlook.com>
2025-08-21 18:48:13 +0100

    Update validPalindrome.js

--- strings/validPalindrome.js
+++ strings/validPalindrome.js
@@ -1,5 +1,5 @@
 function isPalindrome(s) {
-    const cleanedString = s.lowerCase().replace(/[^a-z0-9]/g, '');
+    const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
     let left = 0; 
     let right = cleanedString - 1;
