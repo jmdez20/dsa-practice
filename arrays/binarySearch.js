f80ca50d6329d9f6a3f375933f0a934731594276
jmdez20 <Justinmendez_@outlook.com>
2025-08-24 13:11:36 +0100

    My first Binary Search

---
+++ arrays/binarySearch.js
@@ -0,0 +1,20 @@
+function maxProfit(prices) {
+    if (!prices || prices.length === 0) return false;
+
+    let cheapest_price_so_far = prices[0];
+    let maxProfit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        let currentPrice = prices[i]; 
+
+        if (currentPrice < cheapest_price_so_far) {
+            cheapest_price_so_far = currentPrice;
+        } else {
+            let potentialProfit = currentPrice - cheapest_price_so_far;
+            if (potentialProfit > maxProfit) {
+                maxProfit = potentialProfit
+            }
+        }
+    }
+    return maxProfit;
+}
